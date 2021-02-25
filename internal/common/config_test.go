02b6bea6abbb93ddc3b9d98f40e505e46735cf3a
Andrea Jemmett <1787979+acidghost@users.noreply.github.com>
2021-02-25 16:56:21 +0100

    use single test function TestLoadConfig

--- internal/common/config_test.go
+++ internal/common/config_test.go
@@ -18,61 +18,63 @@ const (
 )
 
 func TestLoadConfig(t *testing.T) {
-	r := bytes.NewReader([]byte(testValidConfigJSON))
-	c, err := loadConfigReader(r)
-	if err != nil {
-		t.Fail()
-	}
-	testutils.AssertEq(t, c.Key, "somekey")
-	testutils.AssertEq(t, c.Storage, "storage.json")
-	testutils.AssertEq(t, c.Zotero, "zotero")
-}
-
-func TestLoadConfigReadErr(t *testing.T) {
-	r := iotest.ErrReader(errors.New("some reader error"))
-	_, err := loadConfigReader(r)
-	if err == nil {
-		t.Fail()
-	}
-}
-
-func TestLoadConfigNotJSON(t *testing.T) {
-	jsonRaw := `{error}`
-	r := bytes.NewReader([]byte(jsonRaw))
-	_, err := loadConfigReader(r)
-	if err == nil {
-		t.Fail()
-	}
-}
-
-func TestLoadConfigInvalid(t *testing.T) {
-	jsonRaw := `{}`
-	r := bytes.NewReader([]byte(jsonRaw))
-	_, err := loadConfigReader(r)
-	if err == nil {
-		t.Fail()
-	}
-	var ec *ErrConfig
-	if errors.As(err, &ec) {
-		found := map[string]bool{"key": false, "zotero": false, "storage": false}
-		for _, err := range ec.errors {
-			switch {
-			case errors.Is(err, ErrConfigEmptyKey):
-				found["key"] = true
-			case errors.Is(err, ErrConfigEmptyStorage):
-				found["storage"] = true
-			case errors.Is(err, ErrConfigEmptyZotero):
-				found["zotero"] = true
-			default:
-				t.Fail()
-			}
+	t.Run("Valid", func(t *testing.T) {
+		r := bytes.NewReader([]byte(testValidConfigJSON))
+		c, err := loadConfigReader(r)
+		if err != nil {
+			t.Fatalf("Unexpected error: %v", err)
 		}
-		for k, v := range found {
-			if !v {
-				t.Fatalf("Error %q not found", k)
-			}
+		testutils.AssertEq(t, c.Key, "somekey")
+		testutils.AssertEq(t, c.Storage, "storage.json")
+		testutils.AssertEq(t, c.Zotero, "zotero")
+	})
+	t.Run("Read error", func(t *testing.T) {
+		expErr := errors.New("some reader error")
+		r := iotest.ErrReader(expErr)
+		_, err := loadConfigReader(r)
+		if err == nil {
+			t.Fatalf("Expected error")
+		} else if !errors.Is(err, expErr) {
+			t.Fatalf("Got wrong error")
 		}
-	} else {
-		t.Fail()
-	}
+	})
+	t.Run("Invalid JSON", func(t *testing.T) {
+		jsonRaw := `{error}`
+		r := bytes.NewReader([]byte(jsonRaw))
+		_, err := loadConfigReader(r)
+		if err == nil {
+			t.Fatalf("Expected error")
+		}
+	})
+	t.Run("Invalid config", func(t *testing.T) {
+		jsonRaw := `{}`
+		r := bytes.NewReader([]byte(jsonRaw))
+		_, err := loadConfigReader(r)
+		if err == nil {
+			t.Fatalf("Expected error")
+		}
+		var ec *ErrConfig
+		if errors.As(err, &ec) {
+			found := map[string]bool{"key": false, "zotero": false, "storage": false}
+			for _, err := range ec.errors {
+				switch {
+				case errors.Is(err, ErrConfigEmptyKey):
+					found["key"] = true
+				case errors.Is(err, ErrConfigEmptyStorage):
+					found["storage"] = true
+				case errors.Is(err, ErrConfigEmptyZotero):
+					found["zotero"] = true
+				default:
+					t.Fail()
+				}
+			}
+			for k, v := range found {
+				if !v {
+					t.Fatalf("Error %q not found", k)
+				}
+			}
+		} else {
+			t.Fail()
+		}
+	})
 }
